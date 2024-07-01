c2ca60f140602552604ca8d163fb0de336657243
Shahbaz Ahmad <Sa798027@gmail.com>
2024-07-01 23:45:32 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Weather from './Weather';
+import './App.css';
+const App = () => {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
